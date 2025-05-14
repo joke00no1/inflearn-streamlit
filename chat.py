f59425a3e68cbfc9a585ee6c81779061ac5931ab
서윤정 <joke00no1@gmail.com>
2025-05-14 11:13:57 +0900

    add few shots

---
+++ chat.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+from dotenv import load_dotenv
+from llm import get_ai_response
+
+st.set_page_config(page_title="소득세 챗봇" , page_icon=":sunglasses:")
+
+
+st.title(":sunglasses: 소득세 챗봇")
+st.caption("소득세에 관련된 모든것을 답해드립니다!")
+
+load_dotenv()
+
+# Session State also supports attribute based syntax
+if 'message_list' not in st.session_state:
+    st.session_state.message_list = []
+
+for message in st.session_state.message_list:
+   with st.chat_message(message["role"]):
+      st.write(message["content"])
+   
+
+
+
+if user_question := st.chat_input("소득세에 관련한 질문을 입력해 주세요!"):
+    with st.chat_message("user"):
+        st.write(user_question)
+    st.session_state.message_list.append({"role":"user", "content":user_question})
+
+    #로딩바 
+    with st.spinner("답변을 생성하는 중입니다."):
+        ai_response = get_ai_response(user_question)
+        with st.chat_message("ai"):
+            ai_message = st.write_stream(ai_response)
+            st.session_state.message_list.append({"role":"ai", "content":ai_message})
+
